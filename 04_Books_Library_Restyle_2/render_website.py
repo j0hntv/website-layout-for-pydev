075759754ed4a60df9464f8c08a9c0bf11525c01
J0hn <j0hntvdesign@gmail.com>
2020-05-09 18:11:04 +0300

    Update render_website.py

--- 04_Books_Library_Restyle_2/render_website.py
+++ 04_Books_Library_Restyle_2/render_website.py
@@ -1,6 +1,5 @@
 import json
 import os
-from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from more_itertools import chunked
 from livereload import Server
